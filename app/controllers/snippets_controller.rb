76e8952512d6fd7137cfffc752fc76f41fa7cf88
Jeremy Cerise <jcerise06@gmail.com>
2014-01-17 18:27:31 -0700

    Added Show action and basic show template for Snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -6,12 +6,20 @@ class SnippetsController < ApplicationController
 
 	def create
 		@snippet = Snippet.new(snippet_params)
+		@snippet.user_id = current_user.id
 		if @snippet.save
 			# Hanlde a successful save
+			redirect_to @snippet
 		else
 			render 'new'
 		end
 	end
+
+	def show
+		@snippet = Snippet.find(params[:id])
+	end
+
+	private
 
 	##
 	# Set the paramaters to permit the HTTP request
