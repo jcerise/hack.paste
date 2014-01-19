bef607eeced6509855d265b6e1101208ad82f2a2
Jeremy Cerise <jcerise06@gmail.com>
2014-01-18 22:36:58 -0700

    Added some styling and additional componentry to viewing snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -2,7 +2,7 @@ class SnippetsController < ApplicationController
 
 	def new
 		@snippet = Snippet.new
-		@languages = ['HTML', 'CSS', 'Javascript', 'Java', 'PHP', 'Coffeescript',
+		@languages = ['CSS', 'Javascript', 'Java', 'PHP', 'Coffeescript',
 						'Sass (Scss)', 'Bash', 'C', 'C++', 'Python', 'SQL', 'HTTP',
 					    'Ruby', 'C#', 'Go']
 	end
@@ -14,7 +14,7 @@ class SnippetsController < ApplicationController
 			# Handle a successful save
 			redirect_to @snippet
 		else
-			@languages = ['HTML', 'CSS', 'Javascript', 'Java', 'PHP', 'Coffeescript',
+			@languages = ['Markup', 'CSS', 'Javascript', 'Java', 'PHP', 'Coffeescript',
 						'Sass (Scss)', 'Bash', 'C', 'C++', 'Python', 'SQL', 'HTTP',
 					    'Ruby', 'C#', 'Go']
 			render 'new'
@@ -23,7 +23,8 @@ class SnippetsController < ApplicationController
 
 	def show
 		@snippet = Snippet.find(params[:id])
-		@snippet.code = '<pre class="line-numbers"><code class="language-ruby">' + @snippet.code + '</code></pre>'
+		language = @snippet.language.downcase
+		@snippet.code = '<pre class="line-numbers code-block"><code class="language-' + language + '">' + @snippet.code + '</code></pre>'
 
 		rescue ActiveRecord::RecordNotFound
   			redirect_to '/', :alert => "Record not found..."
